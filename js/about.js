fff0882a7151ea7833b2d81b22679b210666e0b0
adiarim <adiarim@example.com>
2026-01-17 02:08:15 +0600

    slider

--- js/about.js
+++ js/about.js
@@ -75,3 +75,91 @@ window.addEventListener('mousemove', (e) => {
     magicSpotlight.style.left = e.clientX + 'px';
     magicSpotlight.style.top = e.clientY + 'px';
 });
+
+
+const WILDLIFE_API = [
+  {
+    id: 1,
+    name: "Great Leonopteryx",
+    naviName: "Toruk",
+    image: "./images/fauna-flora/toruk.jpg"
+  },
+  {
+    id: 2,
+    name: "Tulkun",
+    naviName: "Tulkun",
+    image: "./images/fauna-flora/TulkunPhoto.jpg"
+  },
+  {
+    id: 3,
+    name: "The Hometree",
+    naviName: "Kelutral",
+    image: "./images/fauna-flora/hometrhee.jpg"
+  },
+  {
+    id: 4,
+    name: "Ilu",
+    naviName: "Marine Mount of the Metkayina Clan",
+    image: "./images/fauna-flora/Ilu_claimPhoto.jpg"
+  },
+  {
+    id: 5,
+    name: "Tree of Souls",
+    naviName: "Vitraya Ramunong",
+    image: "./images/fauna-flora/threeAvatar.jpg"
+  },
+  {
+    id: 6,
+    name: "Tithonethere",
+    naviName: "Tithonethere",
+    image: "./images/fauna-flora/fauna-tithonethere.webp"
+  },
+  {
+    id: 7,
+    name: "Vanraykan",
+    naviName: "Kelutral",
+    image: "./images/fauna-flora/VunraykanPhoto.jpg"
+  }
+];
+
+const MAX_WILDLIFE = WILDLIFE_API.length;
+let wildlifeId = 1;
+
+const wildlifeCard = document.querySelector("#wildlife-card");
+const nextWildBtn = document.querySelector("#btn-next-wild");
+const prevWildBtn = document.querySelector("#btn-prev-wild");
+
+const wildlifeSwitcher = (id) => {
+    const data = WILDLIFE_API.find(item => item.id === id);
+
+    if (!data) {
+        wildlifeCard.innerHTML = `<p style="color: red">Creature not found</p>`;
+    } else {
+        const { name, naviName, image } = data;
+        
+        wildlifeCard.innerHTML = `
+            <div class="card-content">
+                <div class="card-img-box">
+                    <img src="${image}" alt="${name}">
+                </div>
+                <div class="card-info">
+                    <h3>${name}</h3>
+                    <h4 style="color: var(--blue)">${naviName}</h4>
+                </div>
+            </div>
+        `;
+    }
+}
+
+wildlifeSwitcher(wildlifeId);
+
+
+nextWildBtn.addEventListener('click', () => {
+    wildlifeId = (wildlifeId >= MAX_WILDLIFE) ? 1 : wildlifeId + 1;
+    wildlifeSwitcher(wildlifeId);
+});
+
+prevWildBtn.addEventListener('click', () => {
+    wildlifeId = (wildlifeId <= 1) ? MAX_WILDLIFE : wildlifeId - 1;
+    wildlifeSwitcher(wildlifeId);
+});
